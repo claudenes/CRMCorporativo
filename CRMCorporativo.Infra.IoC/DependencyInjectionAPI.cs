48956a0f4a49d34f617c320a37cc37fdcc3edb76
Claudenes Almeida <claudenes@gmail.com>
2026-02-19 20:20:19 -0300

    Implementacao da Camada IoC

---
+++ CRMCorporativo.Infra.IoC/DependencyInjectionAPI.cs
@@ -0,0 +1,44 @@
+﻿using CRMCorporativo.Application.Interfaces;
+using CRMCorporativo.Application.Mappings;
+using CRMCorporativo.Application.Services;
+using CRMCorporativo.Domain.Account;
+using CRMCorporativo.Domain.Interfaces;
+using CRMCorporativo.Infra.Data.Context;
+using CRMCorporativo.Infra.Data.Identity;
+using CRMCorporativo.Infra.Data.Repositories;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+namespace CRMCorporativo.Infra.IoC;
+
+public static class DependencyInjectionAPI
+{
+    public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+         options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"
+        ), b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+        services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+        services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+        services.AddScoped<IClienteRepository, ClienteRepository>();
+        services.AddScoped<IClienteService, ClienteService>();
+        services.AddScoped<IEnderecoService, EnderecoService>();
+
+        services.AddScoped<IAuthenticate, AuthenticateService>();
+
+        services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+        var myhandlers = AppDomain.CurrentDomain.Load("CRMCorporativo.Application");
+        //services.AddMediatR(myhandlers);
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(myhandlers));
+
+        return services;
+    }
+}
