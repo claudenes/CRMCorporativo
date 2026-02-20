2d80cfbe8df334026def7cff207f80aaae00a0e9
Claudenes Almeida <claudenes@gmail.com>
2026-02-19 21:16:27 -0300

    Implementacao de camada Web.UI

---
+++ CRMCorporativo.WebUI/Controllers/ClientesController.cs
@@ -0,0 +1,115 @@
+﻿using CRMCorporativo.Application.DTOs;
+using CRMCorporativo.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CRMCorporativo.WebUI.Controllers;
+
+public class ClientesController : Controller
+{
+    private readonly IClienteService _clienteService;
+    private readonly IEnderecoService _enderecoService;
+    private readonly IWebHostEnvironment _environment;
+
+    public ClientesController(IClienteService clienteAppService,
+        IEnderecoService enderecoService, IWebHostEnvironment environment)
+    {
+        _clienteService = clienteAppService;
+        _enderecoService = enderecoService;
+        _environment = environment;
+
+
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var clientes = await _clienteService.GetClientes();
+        return View(clientes);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Create()
+    {
+        ViewBag.CategoryId =
+        new SelectList(await _enderecoService.GetEnderecos(), "Id", "Logradouro");
+
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(ClienteDTO clienteDto)
+    {
+        if (ModelState.IsValid)
+        {
+            await _clienteService.Add(clienteDto);
+            return RedirectToAction(nameof(Index));
+        }
+        else
+        {
+            ViewBag.CategoryId =
+                        new SelectList(await _enderecoService.GetEnderecos(), "Id", "Logradouro");
+        }
+        return View(clienteDto);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null) return NotFound();
+        var clienteDto = await _clienteService.GetById(id);
+
+        if (clienteDto == null) return NotFound();
+
+        var enderecos = await _enderecoService.GetEnderecos();
+        ViewBag.EnderecoId = new SelectList(enderecos, "Id", "Logradouro", clienteDto.EnderecoId);
+
+        return View(clienteDto);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(ClienteDTO clienteDto)
+    {
+        if (ModelState.IsValid)
+        {
+            await _clienteService.Update(clienteDto);
+            return RedirectToAction(nameof(Index));
+        }
+        return View(clienteDto);
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpGet]
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+            return NotFound();
+
+        var clienteDto = await _clienteService.GetById(id);
+
+        if (clienteDto == null) return NotFound();
+
+        return View(clienteDto);
+    }
+
+    [HttpPost(), ActionName("Delete")]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        await _clienteService.Remove(id);
+        return RedirectToAction("Index");
+    }
+
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null) return NotFound();
+        var clienteDto = await _clienteService.GetById(id);
+
+        if (clienteDto == null) return NotFound();
+        var wwwroot = _environment.WebRootPath;
+       
+     
+
+        return View(clienteDto);
+    }
+}
---
+++ CRMCorporativo.WebUI/Controllers/EnderecosController.cs
@@ -0,0 +1,100 @@
+﻿using CRMCorporativo.Application.DTOs;
+using CRMCorporativo.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRMCorporativo.WebUI.Controllers;
+
+[Authorize]
+public class EnderecosController : Controller
+{
+    private readonly IEnderecoService _enderecoService;
+    public EnderecosController(IEnderecoService enderecoService)
+    {
+        _enderecoService = enderecoService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var enderecos = await _enderecoService.GetEnderecos();
+        return View(enderecos);
+    }
+
+    [HttpGet()]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(EnderecoDTO enderecos)
+    {
+        if (ModelState.IsValid)
+        {
+            await _enderecoService.Add(enderecos);
+            return RedirectToAction(nameof(Index));
+        }
+        return View(enderecos);
+    }
+
+    [HttpGet()]
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null) return NotFound();
+        var enderecoDto = await _enderecoService.GetById(id);
+        if (enderecoDto == null) return NotFound();
+        return View(enderecoDto);
+    }
+
+    [HttpPost()]
+    public async Task<IActionResult> Edit(EnderecoDTO enderecoDto)
+    {
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                await _enderecoService.Update(enderecoDto);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(enderecoDto);
+    }
+
+    [HttpGet()]
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+            return NotFound();
+
+        var enderecoDto = await _enderecoService.GetById(id);
+
+        if (enderecoDto == null) return NotFound();
+
+        return View(enderecoDto);
+    }
+
+    [HttpPost(), ActionName("Delete")]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        await _enderecoService.Remove(id);
+        return RedirectToAction("Index");
+    }
+
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+            return NotFound();
+
+        var enderecoDto = await _enderecoService.GetById(id);
+
+        if (enderecoDto == null)
+            return NotFound();
+
+        return View(enderecoDto);
+    }
+}
